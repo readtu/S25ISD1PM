817dc2a1805457688d973bef1413ca60ddc0df17
Nat Riddle <nattheriddle1@gmail.com>
2025-02-11 13:45:38 -0500

    Setup templates and static files

--- chairs_app/urls.py
+++ chairs_app/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import path
+
+from chairs_app import views
+
+urlpatterns = [
+    path("", views.home, name=views.home.__name__),
+]
--- chairs_project/urls.py
+++ chairs_project/urls.py
@@ -1,7 +1,9 @@
 """URL configuration for chairs_project project."""
 
+from django.contrib.staticfiles.urls import urlpatterns
 from django.urls import include, path
 
 urlpatterns = [
     path("", include("chairs_app.urls")),
+    *urlpatterns,
 ]
