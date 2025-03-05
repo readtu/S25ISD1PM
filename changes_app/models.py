85b8d9d4b1faf74c69d618bb9514a1ddece6740f
Nat Riddle <nattheriddle1@gmail.com>
2025-03-05 12:03:10 -0500

    More changes for implementing Changes

--- changes_app/models.py
+++ changes_app/models.py
@@ -1,6 +1,10 @@
 from django.db.models import Model
+from django.db.models.deletion import SET_NULL
 from django.db.models.enums import TextChoices
-from django.db.models.fields import UUIDField
+from django.db.models.fields import CharField, UUIDField
+from django.db.models.fields.related import ForeignKey
+
+from semesters_app.models import Semester
 
 
 class ChangeStatus(TextChoices):
@@ -11,4 +15,10 @@ class ChangeStatus(TextChoices):
 
 class Change(Model):
     uuid = UUIDField(primary_key=True)
-
+    status = CharField(max_length=10, choices=ChangeStatus.choices, blank=True, default="pending")
+    semester = ForeignKey(
+        Semester,
+        on_delete=SET_NULL,
+        related_name="changes",
+        null=True,
+    )
--- changes_app/views.py
+++ changes_app/views.py
@@ -1,18 +1,46 @@
 # ruff: noqa: D103
 
+from django.contrib.messages import success
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_GET, require_POST
+
+from changes_app.models import Change, ChangeStatus
 
 
 @require_GET
 def list_changes(request: HttpRequest) -> HttpResponse:
-    return render(request, f"changes_app/{list_changes.__name__}.html", {})
+    group_by_semester = request.GET.get("group_by", "none") == "semester"
+    status = request.GET.get("status", "any")
+    changes = Change.objects.all()
+    if status in ChangeStatus:
+        changes = changes.filter(status=status)
+    return render(
+        request,
+        f"changes_app/{list_changes.__name__}.html",
+        {
+            "group_by_semester": group_by_semester,
+            "status": status,
+            "changes": changes,
+        },
+    )
 
 
 @require_POST
-def accept_change(request: HttpRequest, uuid: str) -> HttpResponse: ...
+def accept_change(request: HttpRequest, uuid: str) -> HttpResponse:
+    change = get_object_or_404(Change, uuid=uuid)
+    # TODO: accept the change
+    change.status = ChangeStatus.ACCEPTED
+    change.save()
+    success(request, f"Accepted {change}.")
+    return redirect("list_changes")
 
 
 @require_POST
-def reject_change(request: HttpRequest, uuid: str) -> HttpResponse: ...
+def reject_change(request: HttpRequest, uuid: str) -> HttpResponse:
+    change = get_object_or_404(Change, uuid=uuid)
+    # TODO: reject the change
+    change.status = ChangeStatus.REJECTED
+    change.save()
+    success(request, f"Rejected {change}.")
+    return redirect("list_changes")
