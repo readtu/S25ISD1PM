4df698c31c2f7346c040abeffa4882a72fcc0690
Nat Riddle <nattheriddle1@gmail.com>
2025-03-05 14:39:45 -0500

    UWSGI config

--- chairs_project/settings.py
+++ chairs_project/settings.py
@@ -105,6 +105,9 @@ TIME_ZONE = "UTC"
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static/"
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
