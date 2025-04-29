1c3324c622977f7eb96185aef1dde81f99b2211e
Nat Riddle <nattheriddle1@gmail.com>
2025-04-29 12:57:10 -0400

    Updated class schema

--- classes_app/models.py
+++ classes_app/models.py
@@ -2,19 +2,22 @@ from uuid import uuid4
 
 from django.db.models import Model
 from django.db.models.deletion import CASCADE, SET_NULL
-from django.db.models.fields import CharField, UUIDField
+from django.db.models.fields import BooleanField, CharField, UUIDField
 from django.db.models.fields.related import ForeignKey
 
-from departments_app.models import Department
+from departments_app.models import Subject
+from locations_app.models import Room
 from semesters_app.models import Semester
 
 
 class Class(Model):
     uuid = UUIDField(primary_key=True, default=uuid4)
     name = CharField(max_length=255)
-    department = ForeignKey(Department, on_delete=SET_NULL, related_name="classes", null=True)
+    subject = ForeignKey(Subject, on_delete=SET_NULL, related_name="classes", null=True)
+    room = ForeignKey(Room, on_delete=SET_NULL, related_name="classes", null=True)
     semester = ForeignKey(Semester, on_delete=CASCADE, related_name="classes")
     code = CharField(max_length=10)
+    is_suggestion = BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.name
